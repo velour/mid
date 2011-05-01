511d84436aaa8df4130cc5ba88ef2780ccea3da0
Steve McCoy <stk5@cs.unh.edu>
2011-04-30 23:55:38 -0400

    Crappy movement, just to try it out.

--- main.c
+++ main.c
@@ -1,8 +1,5 @@
 #include <SDL/SDL.h>
 
-const int w = 512;
-const int h = 512;
-
 int main(int argc, char *argv[]){
 	SDL_Window* window;
         SDL_Renderer* renderer;
@@ -11,31 +8,39 @@ int main(int argc, char *argv[]){
                 return 1;
 
         window = SDL_CreateWindow("SDL_RenderClear",
-				  SDL_WINDOWPOS_CENTERED,
-				  SDL_WINDOWPOS_CENTERED,
-				  w, h,
-				  SDL_WINDOW_SHOWN);
+                        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                        512, 512,
+                        SDL_WINDOW_SHOWN);
 
         renderer = SDL_CreateRenderer(window, -1, 0);
+
+	int x = 0, y = 0;
 
 	for(;;){
 		SDL_Event event;
 		while(SDL_PollEvent(&event)){
 			switch(event.type){
 			case SDL_KEYDOWN:
-				SDL_Quit();
-				return 0;
+				switch(event.key.keysym.sym){
+				case SDLK_LEFT: x--; break;
+				case SDLK_RIGHT: x++; break;
+				case SDLK_UP: y--; break;
+				case SDLK_DOWN: y++; break;
+				default:
+					SDL_Quit();
+					return 0;
+				}
 			}
 		}
 
 	        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-
+	
 	        SDL_RenderClear(renderer);
-
+	
 		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
-		SDL_Rect r = {0, 0, 10, 10};
+		SDL_Rect r = {x, y, 10, 10};
 		SDL_RenderDrawRect(renderer, &r);
-
+	
 	        SDL_RenderPresent(renderer);
 
         	SDL_Delay(50);
