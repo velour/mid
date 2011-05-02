b6d2c6cfca1c100686a90b512765026a2e0cbfd0
Ethan Burns <burns.ethan@gmail.com>
2011-05-02 19:59:18 -0400

    Remove whitespace for some better grouping.

--- include/mid.h
+++ include/mid.h
@@ -125,9 +125,7 @@ void scrnrun(Scrnstk *, Gfx *);
 
 /* Concatinate path names.  'cat' must be of size PATH_MAX + 1. */
 void fscat(const char *d, const char *f, char cat[]);
-
 bool isdir(const char *p);
-
 /* Find a file in the subtree rooted by 'root'.  'out' is the
  * resulting file, it must be of size PATH_MAX + 1. */
 bool fsfind(const char *root, const char *fname, char out[]);
