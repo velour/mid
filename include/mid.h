8acbd8550601b0cd65531f7839765a9038e1408e
Ethan Burns <burns.ethan@gmail.com>
2011-05-02 20:10:01 -0400

    Remove dependency on stdbool.h from mid.h

--- include/mid.h
+++ include/mid.h
@@ -1,4 +1,3 @@
-#include <stdbool.h>
 #include <limits.h>
 /* ick. */
 #if !defined(PATH_MAX)
@@ -125,10 +124,10 @@ void scrnrun(Scrnstk *, Gfx *);
 
 /* Concatinate path names.  'cat' must be of size PATH_MAX + 1. */
 void fscat(const char *d, const char *f, char cat[]);
-bool isdir(const char *p);
+_Bool isdir(const char *p);
 /* Find a file in the subtree rooted by 'root'.  'out' is the
  * resulting file, it must be of size PATH_MAX + 1. */
-bool fsfind(const char *root, const char *fname, char out[]);
+_Bool fsfind(const char *root, const char *fname, char out[]);
 
 enum {
 /* Number of items to allow in the cache. */
@@ -140,7 +139,7 @@ enum {
 typedef struct {
 	void *data;
 	char file[PATH_MAX + 1];
-	bool del;
+	_Bool del;
 	int seq;
 	int ind;
 } Resrc;
