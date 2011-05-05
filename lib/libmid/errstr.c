4beff6e58ce230e76622eba03182852c629679d7
Steve McCoy <stk5@cs.unh.edu>
2011-05-05 05:00:01 -0400

    Those are just macros for SDL_GetError()...

--- lib/libmid/errstr.c
+++ lib/libmid/errstr.c
@@ -1,7 +1,5 @@
 #include "../../include/mid.h"
 #include <SDL/SDL_error.h>
-#include <SDL/SDL_image.h>
-#include <SDL/SDL_ttf.h>
 #include <errno.h>
 #include <string.h>
 
@@ -11,14 +9,6 @@ const char *miderrstr(void){
 	int err = errno;
 
 	const char *e = SDL_GetError();
-	if(e[0] != '\0')
-		return e;
-
-	e = IMG_GetError();
-	if(e[0] != '\0')
-		return e;
-
-	e = TTF_GetError();
 	if(e[0] != '\0')
 		return e;
 
