d2262c3abbddb6649d816cec547e13bf35eead88
Ethan Burns <burns.ethan@gmail.com>
2011-05-15 19:24:06 -0400

    Remove broken trace code.

--- include/mid.h
+++ include/mid.h
@@ -32,7 +32,6 @@ Point rectdist(Rect a, Rect b);
 /* Trace the movement of a along the vector v (dx, dy).  If a will
  * intersect with b, then the result is the new vector (dx, dy) that
  * respects the collision. */
-Point recttrace1(Rect a, Point v, Rect b);
 void ptmv(Point *, float dx, float dy);
 
 typedef struct Isect Isect;
