b19d1c73e1383b29b1cd528cab78246d742d91bb
Steve McCoy <stk5@cs.unh.edu>
2011-05-20 08:46:30 -0400

    Need to add this.

---
+++ cmd/mid/keymap.c
@@ -0,0 +1,28 @@
+#include "../../include/mid.h"
+#include "game.h"
+#include <stdio.h>
+#include <string.h>
+
+_Bool keymapread(char km[], char *fname){
+	FILE *f = fopen(fname, "r");
+	if(!f)
+		return 1;
+
+	char act[8];
+	char k;
+	while(fscanf(f, "%7s %c", act, &k) != EOF){
+		if(strcmp(act, "left") == 0)
+			km[Mvleft] = k;
+		else if(strcmp(act, "right") == 0)
+			km[Mvright] = k;
+		else if(strcmp(act, "jump") == 0)
+			km[Mvjump] = k;
+	}
+
+	if(ferror(f)){
+		fclose(f);
+		return 1;
+	}
+	fclose(f);
+	return 0;
+}
