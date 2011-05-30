e8cfa3e4cffd7364c6122c312eaf1f6a131f8848
Ethan Burns <burns.ethan@gmail.com>
2011-05-30 13:49:23 -0700

    Point to correct images.

---
+++ resrc/leftanims.sh
@@ -0,0 +1,16 @@
+#/bin/sh
+#
+# Convert an animations facing right to animations facing left.
+#
+
+DIR=$1
+for APATH in $DIR/right/*; do
+    ANIM=`basename $APATH`
+    mkdir -p $DIR/left/$ANIM
+    cp $DIR/right/$ANIM/*.png $DIR/left/$ANIM/
+    for PNG in $DIR/left/$ANIM/*.png; do
+	convert $PNG -flop $PNG~
+	mv $PNG~ $PNG
+	cat $DIR/right/$ANIM/anim | sed 's/right/left'/g > $DIR/left/$ANIM/anim
+    done
+done
