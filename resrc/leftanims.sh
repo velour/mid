39b21edeaf92296ef6f6851301ddf9683f31eafb
Ethan Burns <burns.ethan@gmail.com>
2011-05-30 13:56:20 -0700

    Don't make left animations where some already exist.

--- resrc/leftanims.sh
+++ resrc/leftanims.sh
@@ -6,11 +6,20 @@
 DIR=$1
 for APATH in $DIR/right/*; do
     ANIM=`basename $APATH`
+
+    test -e $DIR/left/$ANIM && {
+	echo Skipping $DIR/left/$ANIM
+	continue
+    }
+
+    echo Creating $DIR/left/$ANIM
     mkdir -p $DIR/left/$ANIM
     cp $DIR/right/$ANIM/*.png $DIR/left/$ANIM/
+
     for PNG in $DIR/left/$ANIM/*.png; do
 	convert $PNG -flop $PNG~
 	mv $PNG~ $PNG
 	cat $DIR/right/$ANIM/anim | sed 's/right/left'/g > $DIR/left/$ANIM/anim
     done
+
 done
