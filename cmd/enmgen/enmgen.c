cae1d7b848544a691717cdbfdb2b47ce2c6b0c4c
Ethan Burns <burns.ethan@gmail.com>
2011-07-24 22:17:11 -0700

    Enemies can't spawn wihin 2 tiles of an up elevator.

--- cmd/enmgen/enmgen.c
+++ cmd/enmgen/enmgen.c
@@ -131,7 +131,7 @@ static _Bool goodloc(Zone *zn, int z, Point pt)
 		&& !zonehasflags(zn, z, pt, (Point) { Twidth, Theight }, Tilecollide)
 		&& !zonehasflags(zn, z, (Point) { pt.x - doorrad, pt.y },
 			(Point) { 2 * doorrad * Twidth, Theight },
-			Tilefdoor | Tilebdoor )
+			Tilefdoor | Tilebdoor | Tileup)
 		&& zoneongrnd(zn, z, pt, (Point) { Twidth, Theight })
 		&& !zoneoverlap(zn, z, pt, (Point) { Twidth, Theight });
 }
