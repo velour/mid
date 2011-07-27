dd5ce31c6f5e5359404b31d49a624e27f200b168
Steve McCoy <mccoyst@gmail.com>
2011-07-26 21:26:02 -0400

    I failed to include this vital file.

---
+++ lib/mid/splat.c
@@ -0,0 +1,80 @@
+// Copyright © 2011 Steve McCoy and Ethan Burns
+// Licensed under the MIT License. See LICENSE for details.
+#include "../../include/mid.h"
+#include "enemy.h"
+#include <stdio.h>
+#include <assert.h>
+#include "../../include/log.h"
+Img *splatimg;
+
+typedef struct Splat Splat;
+struct Splat{
+	Anim anim;
+};
+
+_Bool splatinit(Enemy *e, int x, int y){
+	e->hp = 600/Ticktm;
+
+	Splat *sp = xalloc(1, sizeof(*sp));
+	sp->anim = (Anim){
+		.row = 0,
+		.len = 3,
+		.delay = 200/Ticktm,
+		.w = 32,
+		.h = 32,
+		.d = 200/Ticktm
+	};
+
+	e->data = sp;
+	return 1;
+}
+
+void splatfree(Enemy *e){
+	xfree(e->data);
+}
+
+void splatupdate(Enemy *e, Player *p, Lvl *l){
+	bodyupdate(&e->b, l);
+
+	if(!splatimg) splatimg = resrcacq(imgs, "img/splat.png", 0);
+
+	Splat *sp = e->data;
+	sp->anim.sheet = splatimg;
+	animupdate(&sp->anim);
+
+	e->hp--;
+	if(e->hp <= 0)
+		e->id = 0;
+}
+
+void splatdraw(Enemy *e, Gfx *g){
+	Splat *sp = e->data;
+	if(!splatimg) splatimg = resrcacq(imgs, "img/splat.png", 0);
+	sp->anim.sheet = splatimg;
+	camdrawanim(g, &sp->anim, e->b.bbox.a);
+}
+
+_Bool splatscan(char *buf, Enemy *e){
+	if(!defaultscan(buf, e))
+		return 0;
+
+	e->hitback = 0;
+	e->iframes = 0;
+
+	Splat *sp = xalloc(1, sizeof(*sp));
+	sp->anim = (Anim){
+		.row = 0,
+		.len = 3,
+		.delay = 200/Ticktm,
+		.w = 32,
+		.h = 32,
+		.d = 200/Ticktm
+	};
+
+	e->data = sp;
+	return 1;
+}
+
+_Bool splatprint(char *buf, size_t sz, Enemy *e){
+	return defaultprint(buf, sz, e);
+}
