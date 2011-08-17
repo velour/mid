c3522e943e03b7ddb827c5e9492632d8b7cf1ec7
Steve McCoy <mccoyst@gmail.com>
2011-08-17 18:22:25 -0400

    Add more info to the title screen. Fixes issue 65.

--- cmd/mid/title.c
+++ cmd/mid/title.c
@@ -7,7 +7,13 @@
 typedef struct Tit Tit;
 struct Tit{
 	Img *title;
+	Img *start;
+	Img *opts;
+	Img *copy;
 	Point titlepos;
+	Point startpos;
+	Point optspos;
+	Point copypos;
 };
 
 static void update(Scrn*,Scrnstk*);
@@ -30,9 +36,46 @@ Scrn *titlescrnnew(Gfx *g){
 	if(!t.title)
 		return NULL;
 
+	Txtinfo ti = { 32, {0} };
+	Txt *f = resrcacq(txt, "txt/retganon.ttf", &ti);
+	if(!f)
+		return NULL;
+
+	t.start = txt2img(g, f, "Press '%c' to Start", kmap[Mvinv]);
+	if(!t.start)
+		return NULL;
+
+	t.opts = txt2img(g, f, "Press '%c' for Options", kmap[Mvact]);
+	if(!t.opts){
+		imgfree(t.start);
+		return NULL;
+	}
+
+	t.copy = txt2img(g, f, "Copyright 2011 Steve McCoy and Ethan Burns");
+	if(!t.copy){
+		imgfree(t.opts);
+		imgfree(t.start);
+		return NULL;
+	}
+
 	t.titlepos = (Point){
 		gfxdims(g).x / 2 - imgdims(t.title).x / 2,
 		0
+	};
+
+	t.startpos = (Point){
+		gfxdims(g).x / 2 - imgdims(t.start).x / 2,
+		t.titlepos.y + imgdims(t.title).y
+	};
+
+	t.optspos = (Point){
+		gfxdims(g).x / 2 - imgdims(t.opts).x / 2,
+		t.startpos.y + imgdims(t.start).y + 16
+	};
+
+	t.copypos = (Point){
+		gfxdims(g).x / 2 - imgdims(t.copy).x / 2,
+		gfxdims(g).y - imgdims(t.copy).y - 8
 	};
 
 	s.mt = &titmt;
@@ -47,6 +90,9 @@ static void draw(Scrn *s, Gfx *g){
 	gfxclear(g, (Color){ 240, 240, 240 });
 	Tit *t = s->data;
 	imgdraw(g, t->title, t->titlepos);
+	imgdraw(g, t->start, t->startpos);
+	imgdraw(g, t->opts, t->optspos);
+	imgdraw(g, t->copy, t->copypos);
 	gfxflip(g);
 }
 
@@ -61,8 +107,14 @@ static void handle(Scrn *s, Scrnstk *stk, Event *e){
 		gms.data = g;
 		scrnstkpush(stk, &gms);
 		return;
+	}else if(e->down && e->key == kmap[Mvact]){
+		//TODO
 	}
 }
 
 static void titfree(Scrn *s){
+	Tit *t = s->data;
+	imgfree(t->copy);
+	imgfree(t->opts);
+	imgfree(t->start);
 }
