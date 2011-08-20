14208c783c4ec77b403587f3c9241bb1fcf275a2
Steve McCoy <mccoyst@gmail.com>
2011-08-20 11:53:29 -0400

    Polish up the options screen. Still have to save them to a file.

--- cmd/mid/optscrn.c
+++ cmd/mid/optscrn.c
@@ -9,8 +9,20 @@
 typedef struct Opts Opts;
 struct Opts{
 	char kmap[Nactions];
+	Rect hilit[Nactions];
+	Rect volhilit;
+	_Bool volset;
 	int curkey;
+	Rect okay;
+	_Bool isokay;
+	Rect cancel;
+	_Bool iscancel;
 	Txt *txt;
+	Sfx *testsfx;
+};
+
+enum{
+	Pad = TxtSzMedium/2,
 };
 
 static void update(Scrn *s, Scrnstk *stk);
@@ -36,6 +48,37 @@ Scrn *optscrnnew(void){
 	if(!opts.txt)
 		return NULL;
 
+	opts.testsfx = resrcacq(sfx, "sfx/ow.wav", 0);
+	if(!opts.testsfx)
+		return NULL;
+
+	Point loc = { Pad, Pad };
+	for(int i = Mvleft; i < Nactions; i++){
+		opts.hilit[i] = (Rect){
+			loc,
+			vecadd(loc, (Point){ TxtSzMedium*5, TxtSzMedium })
+		};
+		loc.y = opts.hilit[i].b.y + Pad;
+	}
+
+	loc.y += Pad*2;
+	opts.volhilit = (Rect){
+		loc,
+		vecadd(loc, (Point){ TxtSzMedium*7, TxtSzMedium })
+	};
+
+	loc.y = opts.volhilit.b.y + Pad*3;
+	opts.okay = (Rect){
+		loc,
+		vecadd(loc, (Point){ TxtSzMedium*4, TxtSzMedium })
+	};
+
+	loc.x = opts.okay.b.x + Pad;
+	opts.cancel = (Rect){
+		loc,
+		vecadd(loc, (Point){ TxtSzMedium*4, TxtSzMedium })
+	};
+
 	s.mt = &optsmt;
 	s.data = &opts;
 	return &s;
@@ -59,37 +102,121 @@ static void draw(Scrn *s, Gfx *g){
 
 	gfxclear(g, (Color){ 240, 240, 240 });
 
-	int pad = TxtSzMedium/2;
-	Point loc = { pad, pad };
 	Color hilit = { 255, 219, 0 };
 
 	for(int i = Mvleft; i < Nactions; i++){
-		Point d = txtdims(opt->txt, "%-7s%2c", names[i], opt->kmap[i]);
 		if(i == opt->curkey){
-			Rect hover = { vecadd(loc, (Point){-4,-4}), vecadd(vecadd(loc, d), (Point){4,4}) };
-			gfxfillrect(g, hover, hilit);
+			Rect h = {
+				vecadd(opt->hilit[i].a, (Point){-4,-4}),
+				vecadd(opt->hilit[i].b, (Point){4,4})
+			};
+			gfxfillrect(g, h, hilit);
 		}
-		Point p = txtdraw(g, opt->txt, loc, "%-7s%2c", names[i], opt->kmap[i]);
-		loc.y = p.y + d.y + pad;
+		txtdraw(g, opt->txt, opt->hilit[i].a, "%s", names[i]);
+		Point cd = txtdims(opt->txt, "%c", opt->kmap[i]);
+		cd.x = -cd.x;
+		cd.y = 0;
+		Point cp = vecadd(opt->hilit[i].b, cd);
+		cp.y = opt->hilit[i].a.y;
+		txtdraw(g, opt->txt, cp, "%c", opt->kmap[i]);
 	}
 
-	loc.y += pad*2;
-	txtdraw(g, opt->txt, loc, "Volume: %d", (int)(sndvol(-1) / (float)SndVolMax * 100));
+	if(opt->volset){
+		Rect h = {
+			vecadd(opt->volhilit.a, (Point){-4,-4}),
+			vecadd(opt->volhilit.b, (Point){4,4})
+		};
+		gfxfillrect(g, h, hilit);
+	}
+	txtdraw(g, opt->txt, opt->volhilit.a, "Volume:");
+	float vpcnt = sndvol(-1) / (float)SndVolMax * 100;
+	Point pd = txtdims(opt->txt, "%.1f%%", vpcnt);
+	pd.x = -pd.x;
+	pd.y = 0;
+	Point pp = vecadd(opt->volhilit.b, pd);
+	pp.y = opt->volhilit.a.y;
+	txtdraw(g, opt->txt, pp, "%.1f%%", vpcnt);
+
+	Color butt = { 200, 200, 200 };
+	Rect oh = {
+		vecadd(opt->okay.a, (Point){-4,-4}),
+		vecadd(opt->okay.b, (Point){4,4})
+	};
+	gfxfillrect(g, oh, butt);
+	Point op = {
+		opt->okay.a.x + (opt->okay.b.x - opt->okay.a.x)/2 - txtdims(opt->txt, "OK").x/2,
+		opt->okay.a.y
+	};
+	txtdraw(g, opt->txt, op, "OK");
+	Rect ch = {
+		vecadd(opt->cancel.a, (Point){-4,-4}),
+		vecadd(opt->cancel.b, (Point){4,4})
+	};
+	gfxfillrect(g, ch, butt);
+	Point cp = {
+		opt->cancel.a.x + (opt->cancel.b.x - opt->cancel.a.x)/2 - txtdims(opt->txt, "Cancel").x/2,
+		opt->cancel.a.y
+	};
+	txtdraw(g, opt->txt, cp, "Cancel");
 
 	gfxflip(g);
 }
 
 static void handle(Scrn *s, Scrnstk *stk, Event *e){
-	if(e->type != Keychng || e->repeat || !e->down)
+	Opts *opt = s->data;
+
+	if(e->type == Mousebt && e->down){
+		Point m = { e->x, e->y };
+
+		for(int i = Mvleft; i < Nactions; i++)
+			if(rectcontains(opt->hilit[i], m)){
+				opt->curkey = i;
+				opt->volset = 0;
+				return;
+			}
+
+		if(rectcontains(opt->volhilit, m)){
+			opt->volset = 1;
+			opt->curkey = Nactions;
+			return;
+		}
+
+		if(rectcontains(opt->okay, m)){
+			opt->isokay = 1;
+			scrnstkpop(stk);
+			return;
+		}
+
+		if(rectcontains(opt->cancel, m)){
+			opt->iscancel = 1;
+			scrnstkpop(stk);
+			return;
+		}
+
+		return;
+	}
+
+	if(e->type != Keychng || !e->down)
 		return;
 
 	if(!isalpha(e->key))
 		return;
 
-	Opts *opt = s->data;
+	if(opt->volset){
+		int curvol = sndvol(-1);
+		if(e->key == opt->kmap[Mvleft] && curvol > 0)
+			sndvol(curvol-1);
+		else if(e->key == opt->kmap[Mvright] && curvol < SndVolMax)
+			sndvol(curvol+1);
+		sfxplay(opt->testsfx);
+		return;
+	}
+
+	if(e->repeat)
+		return;
 
 	if(opt->curkey == Nactions){
-		scrnstkpop(stk);
+		opt->curkey = Mvleft;
 		return;
 	}
 
@@ -105,6 +232,15 @@ static void handle(Scrn *s, Scrnstk *stk, Event *e){
 
 static void optsfree(Scrn *s){
 	Opts *opt = s->data;
-	memcpy(kmap, opt->kmap, Nactions);
-	//TODO: save to a file in appdata, also load at game start iff exists
+
+	if(opt->isokay){
+		memcpy(kmap, opt->kmap, Nactions);
+		//TODO: save to a file in appdata, also load at game start iff exists
+	}
+
+	if(opt->iscancel){
+		sndvol(SndVolDefault);
+	}
+
+	memset(opt, 0, sizeof(*opt));
 }
